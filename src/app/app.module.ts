471ce9986715a31d6efdf807a682c7efafc9efaa
Thomas <riukenshin@hotmail.com>
2023-10-11 08:12:02 +0200

    ajout des pages, composants, services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './page/accueil/accueil.component';
+import { JeuComponent } from './page/jeu/jeu.component';
+import { ConnexionComponent } from './component/connexion/connexion.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { ProfilComponent } from './component/profil/profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    JeuComponent,
+    ConnexionComponent,
+    NavBarComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
