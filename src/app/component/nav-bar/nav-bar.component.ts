b2da762700ece3979a4bbb51409b7056ecb163e1
Thomas <riukenshin@hotmail.com>
2023-10-11 08:27:59 +0200

    mise en place du cheminement des composant navbar, profil + html css ts de la nav bar et de la modal profil

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
+  // je déclare ma valeur false pour cacher la fenettrre modal
+  hideProfilModal: boolean = false;
 
+  // au click je change la valeur en true pour aficher la modal
+  openModalProfil() {
+    this.hideProfilModal = true;
+  }
+
+  // je récupere la valeur false envoyer depuis le composant profil pour fermer la fenetre
+  retourDeProfil(valueProfil: boolean) {
+    console.log('La valeur retour', valueProfil);
+    this.hideProfilModal = valueProfil;
+  }
 }
--- src/app/component/profil/profil.component.ts
+++ src/app/component/profil/profil.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
-  styleUrls: ['./profil.component.css']
+  styleUrls: ['./profil.component.css'],
 })
 export class ProfilComponent {
+  // je creer une transmition de mon enfant vers son parent
+  @Output() valueModalProfil: EventEmitter<boolean> =
+    new EventEmitter<boolean>();
 
+  // la methode qui va envoyer l'info false pour fermer la fenettre
+  transmettreValeurProfil() {
+    console.log('profil', this.valueModalProfil.emit(false));
+
+    // j'envoie ça
+    this.valueModalProfil.emit(false);
+  }
 }
